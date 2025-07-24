a90752617f5a3eacb76eb65798dd8a33bc8f03b3
RetricSu <inneverland2013@gmail.com>
2025-07-24 21:13:11 +0800

    some chore

--- src/components/dictation/WrongWordSaveButton.tsx
+++ src/components/dictation/WrongWordSaveButton.tsx
@@ -130,7 +130,7 @@ export const WrongWordSaveButton: React.FC = () => {
             <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
             </svg>
-            <span>Save Wrong Words</span>
+            <span>保存当前页面错词</span>
           </>
         )}
       </button>
